2c87a461ace97438754f70a9420861491eb8aa55
hustlzp <hustlzp@qq.com>
2015-10-25 13:19:40 +0800

    Initial Commit

---
+++ tm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tm
+//
+//  Created by hustlzp on 15/10/25.
+//  Copyright © 2015年 hustlzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tm
+//
+//  Created by hustlzp on 15/10/25.
+//  Copyright © 2015年 hustlzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
